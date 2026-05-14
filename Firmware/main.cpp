68d25cc9d85e99b5444ed99891deeaf5470e8d41
Yuri Capella <142462861+CapellaAgente@users.noreply.github.com>
2026-05-13 21:51:39 -0300

    Implement fan control and update MQTT data format
    Added fan control based on temperature limit and updated MQTT message format.

--- Firmware/main.cpp
+++ Firmware/main.cpp
@@ -7,11 +7,13 @@ const char* wifi_name = "";
 const char* password = "";
 const char* mqtt_server = "";
 
-
 #define DHTTYPE DHT11
 const int DHT_PIN = 33;
-DHT dht(DHT_PIN, DHTTYPE);
+const int PINO_VENTOINHA = 2;
 
+const float TEMP_LIMITE = 28.0; 
+
+DHT dht(DHT_PIN, DHTTYPE);
 WiFiClient espClient;
 PubSubClient client(espClient);
 
@@ -30,16 +32,21 @@ void mqtt_conect() {
   while (!client.connected()) {
     Serial.println("Tentando conexão MQTT...");
     if (client.connect("ESP32_Capella")) {
-      Serial.println("Conectado!");
+      Serial.println("Conectado ao Broker!");
     } else {
+      Serial.print("Falha: ");
+      Serial.print(client.state());
       delay(5000);
     }
   }
-  
 }
 
 void setup() {
   Serial.begin(115200);
+  
+  pinMode(PINO_VENTOINHA, OUTPUT);
+  digitalWrite(PINO_VENTOINHA, LOW); 
+
   setup_wifi();
   client.setServer(mqtt_server, 1883);
   dht.begin();
@@ -53,6 +60,7 @@ void loop() {
 
   static unsigned long lastMessage = 0;
   unsigned long now = millis();
+  
   if (now - lastMessage > 5000) {
     lastMessage = now;
 
@@ -60,9 +68,24 @@ void loop() {
     float temperature = dht.readTemperature();
 
     if (!isnan(temperature) && !isnan(humidity)) {
-      String jsonData = "{\"temp\":" + String(temperature) +  ", \"humi\":" +  String(humidity) + "}";
+      bool statusVentoinha = false;
+      
+      if (temperature >= TEMP_LIMITE) {
+        digitalWrite(PINO_VENTOINHA, HIGH);
+        statusVentoinha = true;
+        Serial.println("[ALERTA] Temperatura alta! Ventoinha LIGADA.");
+      } else {
+        digitalWrite(PINO_VENTOINHA, LOW); 
+        statusVentoinha = false;
+        Serial.println("[OK] Temperatura normal. Ventoinha DESLIGADA.");
+      }
+      
+      String jsonData = "{\"temp\":" + String(temperature) + 
+                        ", \"humi\":" + String(humidity) + 
+                        ", \"fan\":" + String(statusVentoinha ? 1 : 0) + "}";
+      
       client.publish("projeto/smart_it/data", jsonData.c_str());
-      Serial.println("Dados enviados para o broker!");
+      Serial.println("Dados enviados: " + jsonData);
     }
   }
 }
